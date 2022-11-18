aab6580624aaebeaf689585b50a2140766555d6f
phlc <pedrocarvalho@outlook.com>
2022-11-18 11:45:11 -0300

    refacto to use list instead numpy array

--- knights_tour.py
+++ knights_tour.py
@@ -1,10 +1,28 @@
-import numpy as np
+import random
+
+
 
 # Constants
 N = 5   #board size
 P = 8   #population size
 I = 12  #initial position
-SEED = np.arange(0, (N**2))
+SEED = list(range(0, (N**2)))
+
+# Class Organismo
+class Organism():
+
+    # Create Random Organism
+    def __init__(self):
+        tmp = SEED.copy()
+        random.shuffle(tmp)
+        self.org = move_initial(tmp)
+        self.valids = count_valids(self.org)
+
+    def clone(self):
+        new_org = Organism()
+        new_org.org = self.org.copy()
+        new_org.valids = self.valids
+        return new_org
 
 
 # Initialization
@@ -13,24 +31,30 @@ def initialize():
     population = []
 
     for i in range (0, P):
-        tmp = SEED.copy()
-        np.random.shuffle(tmp)
-        tmp = np.delete(tmp, np.where(tmp == I))
-        tmp = np.insert(tmp, 0, 12)
+        tmp = Organism()
         population.append(tmp)
 
     return population
 
 # Selection
 def random_selection(population):
-    new_population = []
+    proportional_population = []
 
+    # proportional population
     for organism in population:
-        for i in range(0, count_valids(organism)):
-            tmp = organism.copy()
-            new_population.append(tmp)
+        for i in range(0, organism.valids):
+            tmp = organism.clone()
+            proportional_population.append(tmp)
 
-    ######
+    # shuffle proportional population
+    random.shuffle(proportional_population)
+
+    # pick new population
+    new_population = []
+    for i in range(0, P):
+        new_population.append(proportional_population[i])
+
+    return new_population
 
 def elitist_selection(population):
     pass
@@ -40,6 +64,12 @@ def elitist_selection(population):
 
 
 # Auxiliary Functions
+# move initial position to front
+def move_initial(p):
+    p.remove(I)
+    p = [I]+p
+    return p
+
 # get 2d coord
 def get_2d(p):
     row = p//N
@@ -76,3 +106,12 @@ def count_valids(p):
         if(is_valid(p[i], p[i+1])):
             count += 1
     return count
+
+
+a = initialize()
+for organism in a:
+    print(organism.org, organism.valids)
+print()
+b = random_selection(a)
+for organism in b:
+    print(organism.org, organism.valids)
